9a12818ed44cbb9552edd0f8e7d4eafc6c4dd169
Russell Aunger <rba@live.com>
2019-07-10 01:55:13 -0400

    Replace tag type switch with map.

--- src/components/UsfmRenderingPlugin.js
+++ src/components/UsfmRenderingPlugin.js
@@ -1,81 +1,111 @@
 import React from "react";
 
+function numberClassNames(node) {
+    const isFront = node.text === "front";
+    const isOne = node.text === "1";
+    return isFront ? "Front" : isOne ? "One" : "";
+}
+
+function destructureTag(node) {
+    const [, pluses, baseTag, number] = node.type.match(/^(\+*)(.*?)(\d*)$/);
+    return {pluses, baseTag, number};
+}
+
+
+
+
+
+
+function ChapterNumberNode(props) {
+    const className = `ChapterNumber ${numberClassNames(props.node)}`;
+    return (
+        <h1 {...props.attributes} className={className}>
+            {props.children}
+        </h1>
+    )
+}
+
+function VerseNumberNode(props) {
+    const className = `VerseNumber ${numberClassNames(props.node)}`;
+    return (
+        <sup {...props.attributes} className={className}>
+            {props.children}
+        </sup>
+    )
+}
+
+function BookIdNode(props) {
+    return (
+        <div {...props.attributes} className="BookId">
+            {props.children}
+        </div>
+    )
+}
+
+function Footnote(props) {
+    return (
+        <div {...props.attributes} className="Footnote">
+            {props.children}
+        </div>
+    )
+}
+
+function Paragraph(props) {
+    return <p {...props.attributes}>{props.children}</p>;
+}
+
+
+
+
+
+const components = new Map([
+    ['id', props =>
+        <BookIdNode {...props}/>
+    ],
+    ['chapterNumber', props =>
+        <ChapterNumberNode {...props}/>
+    ],
+    ['verseNumber', props =>
+        <VerseNumberNode {...props}/>
+    ],
+    ['f', props =>
+        <Footnote {...props}/>
+    ],
+    ['p', props =>
+        <Paragraph {...props}/>
+    ],
+    ['bk', props =>
+        <cite {...props.attributes}>{props.children}</cite>
+    ],
+    ['nd', props =>
+        <span className="NomenDomini" {...props.attributes}>{props.children}</span>
+    ],
+    ['s', props => {
+        const {number} = destructureTag(props.node);
+        if (number == 5 && props.node.text.trim() === "") {
+            // Some editors use \s5 as a chunk delimiter. Separate chunks by horizontal rules.
+            return <hr {...props.attributes} />;
+        } else {
+            const HeadingTag = `h${number || 1}`;
+            return <HeadingTag {...props.attributes}>{props.children}</HeadingTag>;
+        }
+    }],
+    ['r', props =>
+        <h3 {...attributes}><cite>{children}</cite></h3>
+    ]
+]);
+
 export function UsfmRenderingPlugin(options) {
-    function numberClassNames(node) {
-        const isFront = node.text === "front";
-        const isOne = node.text === "1";
-        return isFront ? "Front" : isOne ? "One" : "";
-    }
-
-    function ChapterNumberNode(props) {
-        const className = `ChapterNumber ${numberClassNames(props.node)}`;
-        return (
-            <h1 {...props.attributes} className={className}>
-                {props.children}
-            </h1>
-        )
-    }
-
-    function VerseNumberNode(props) {
-        const className = `VerseNumber ${numberClassNames(props.node)}`;
-        return (
-            <sup {...props.attributes} className={className}>
-                {props.children}
-            </sup>
-        )
-    }
-
-    function BookIdNode(props) {
-        return (
-            <div {...props.attributes} className="BookId">
-                {props.children}
-            </div>
-        )
-    }
-
-    function Footnote(props) {
-        return (
-            <div {...props.attributes} className="Footnote">
-                {props.children}
-            </div>
-        )
-    }
-
     return {
         renderInline: function (props, editor, next) {
-            const [, pluses, baseTag, number] = props.node.type.match(/^(\+*)(.*?)(\d*)$/);
+            const {isFocused, isSelected, attributes, children, node, parent, readOnly, editor: propsEditor} = props;
+            const {pluses, baseTag, number} = destructureTag(node);
 
-            const propsForDom = Object.assign({}, props);
-            delete propsForDom.isFocused;
-            delete propsForDom.isSelected;
-
-            switch (baseTag) {
-                case 'id':
-                    return <BookIdNode {...propsForDom} />;
-                case 'chapterNumber':
-                    return <ChapterNumberNode {...propsForDom} />;
-                case 'verseNumber':
-                    return <VerseNumberNode {...propsForDom} />;
-                case 'f':
-                    return <Footnote {...propsForDom} />;
-                case 'p':
-                    return <p {...propsForDom} />;
-                case 'bk':
-                    return <cite {...propsForDom} />;
-                case 'nd':
-                    return <span className="NomenDomini" {...propsForDom} />;
-                case 's':
-                    if (number == 5 && props.node.text.trim() === "") {
-                        // Some editors use \s5 as a chunk delimiter. Separate chunks by horizontal rules.
-                        return <hr />;
-                    } else {
-                        const HeadingTag = `h${number || 1}`;
-                        return <HeadingTag {...propsForDom} />;
-                    }
-                case 'r':
-                    return <h3 {...props.attributes}><cite>{props.children}</cite></h3>;
-                default:
-                    return next()
+            const componentBuilder = components.get(baseTag);
+            if (componentBuilder) {
+                return componentBuilder(props);
+            } else {
+                return next();
             }
         }
     };
