f7ab1f3e0a480e8c5b86c76cf730b0736b124335
KJoslyn <KJoslyn29@gmail.com>
2019-10-09 15:04:54 -0400

    Hover menu with Section Header button
    The section header button creates a new inline node for a section header, using the currently selected text. Currently this does not hook back into the json source so output usfm is not transformed.

--- src/components/UsfmEditor.js
+++ src/components/UsfmEditor.js
@@ -10,6 +10,7 @@ import {toUsfmJsonAndSlateJson} from "./jsonTransforms/usfmjsToSlate";
 import {handleOperation} from "./operationHandlers";
 import Schema from "./schema";
 import {verseNumberName} from "./numberTypes";
+import {HoverMenu} from "../hoveringMenu/HoveringMenu"
 
 /**
  * A WYSIWYG editor component for USFM
@@ -40,6 +41,48 @@ class UsfmEditor extends React.Component {
         return {usfmJsDocument, value, sourceMap};
     }
 
+    menuRef = React.createRef()
+
+    /**
+     * On update, update the menu.
+     */
+
+    componentDidMount = () => {
+        this.updateMenu()
+    }
+
+    componentDidUpdate = () => {
+        this.updateMenu()
+    }
+
+    /**
+     * Update the menu's absolute position.
+     */
+
+    updateMenu = () => {
+        const menu = this.menuRef.current
+        if (!menu) return
+
+        const { value } = this.state
+        const { fragment, selection } = value
+
+        if (selection.isBlurred || selection.isCollapsed || fragment.text === '') {
+        menu.removeAttribute('style')
+        return
+        }
+
+        const native = window.getSelection()
+        const range = native.getRangeAt(0)
+        const rect = range.getBoundingClientRect()
+        menu.style.opacity = 1
+        menu.style.top = `${rect.top + window.pageYOffset - menu.offsetHeight}px`
+
+        menu.style.left = `${rect.left +
+        window.pageXOffset -
+        menu.offsetWidth / 2 +
+        rect.width / 2}px`
+    }
+
     render = () => {
         return (
             <Editor
@@ -49,9 +92,20 @@ class UsfmEditor extends React.Component {
                 readOnly={false}
                 spellCheck={false}
                 onChange={this.handleChange}
+                renderEditor={this.renderEditor}
             />
         );
     };
+
+    renderEditor = (props, editor, next) => {
+        const children = next()
+        return (
+        <React.Fragment>
+            {children}
+            <HoverMenu ref={this.menuRef} editor={editor} />
+        </React.Fragment>
+        )
+    }
 
     handleChange = (change) => {
         console.info("handleChange", change);
---
+++ src/hoveringMenu/menuComponents.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { cx, css } from 'emotion'
+
+export const Button = React.forwardRef(
+  ({ className, active, reversed, ...props }, ref) => (
+    <span
+      {...props}
+      ref={ref}
+      className={cx(
+        className,
+        css`
+          cursor: pointer;
+          color: ${reversed
+            ? active ? 'white' : '#aaa'
+            : active ? 'black' : '#ccc'};
+        `
+      )}
+    />
+  )
+)
+
+export const Menu = React.forwardRef(({ className, ...props }, ref) => (
+  <div
+    {...props}
+    ref={ref}
+    className={cx(
+      className,
+      css`
+        & > * {
+          display: inline-block;
+        }
+        & > * + * {
+          margin-left: 15px;
+        }
+      `
+    )}
+  />
+))
