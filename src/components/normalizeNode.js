3b856256f93a82c7607e6d687f87121683ce110c
KJoslyn <KJoslyn29@gmail.com>
2019-12-17 13:19:59 -0500

    Generalize naming for merging wrappers

--- src/components/normalizeNode.js
+++ src/components/normalizeNode.js
@@ -1,28 +1,28 @@
 export const Normalize = () => ({
     normalizeNode: (node, editor, next) => {
         if (node.type == "verseBody") {
-            checkAndMergeAdjacentTextWrappers(node.nodes, editor)
+            checkAndMergeAdjacentWrappers(node.nodes, editor)
         }
         return next()
     }
 })
 
-function checkAndMergeAdjacentTextWrappers(nodes, editor) {
+function checkAndMergeAdjacentWrappers(nodes, editor) {
     for (let i = nodes.size - 1; i > 0; i--) {
         const child = nodes.get(i)
         const prev = nodes.get(i - 1)
         if (child.type == "textWrapper" && 
             (prev.type == "textWrapper" || prev.type == "p")) {
-            mergeTextWrappers(child, prev, editor)
+            mergeWrappers(child, prev, editor)
             return
         }
     }
 }
 
-function mergeTextWrappers(wrapper, prevWrapper, editor) {
-    console.info("Merging adjacent textWrappers")
-    console.info("     textWrapper", wrapper.toJS())
-    console.info("     previous textWrapper", prevWrapper.toJS())
+function mergeWrappers(wrapper, prevWrapper, editor) {
+    console.info("Merging adjacent wrappers")
+    console.info("     wrapper", wrapper.toJS())
+    console.info("     previous wrapper", prevWrapper.toJS())
     console.info("     document", editor.value.document.toJS())
 
     const textNode = wrapper.nodes.get(0)
