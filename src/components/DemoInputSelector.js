49169cdf43401a17fce0e982a1535d1b7ca392d1
KevinJoslyn <KJoslyn29@gmail.com>
2020-04-26 15:11:01 -0400

    Add loaded file to dropdown

--- src/components/DemoInputSelector.js
+++ src/components/DemoInputSelector.js
@@ -10,28 +10,38 @@ export const DemoInputSelector = ({ onChange, demoUsfmStrings }) => {
         const reader = new FileReader();
         reader.onload = (e) => {
             onChange(e.target.result)
+            addUsfmFileToDropdown(e.target.fileName, e.target.result)
         }
         return reader
     })()
 
+    const addUsfmFileToDropdown = (fileName, usfm) => {
+        const dropdown = document.getElementById("input-dropdown")
+        const opt = document.createElement("option")
+        opt.key = fileName
+        opt.value = usfm
+        opt.innerHTML = fileName
+        dropdown.appendChild(opt)
+        dropdown.value = usfm
+    }
+
     const handleInputFileChange = 
         event => {
             const file = event.target.files[0]
+            fileReader.fileName = file.name
             fileReader.readAsText(file)
         }
 
     const DropdownMenu = ({ onChange, demoUsfmStrings }) => {
         return (
-            <form>
-                <select required onChange={onChange}>
-                {
-                    Array.from(demoUsfmStrings).map(function(arr) {
-                        const [k, v] = arr;
-                        return <option key={k} value={v}>{k}</option>;
-                    })
-                }
-                </select>
-            </form>
+            <select id="input-dropdown" required onChange={onChange}>
+            {
+                Array.from(demoUsfmStrings).map(function(arr) {
+                    const [k, v] = arr;
+                    return <option key={k} value={v}>{k}</option>;
+                })
+            }
+            </select>
         )
     }
 
@@ -44,7 +54,7 @@ export const DemoInputSelector = ({ onChange, demoUsfmStrings }) => {
 
     const FileSelector = ({ onChange }) => {
         return (
-            <input type="file" accept=".usfm,.txt" onChange={onChange} />
+            <input type="file" class="text-no-display" accept=".usfm,.txt" onChange={onChange} />
         )
     }
 
