6e68bb71fb845b3e0d6c5868886bf9b6e8e78c8b
KJoslyn <KJoslyn29@gmail.com>
2020-07-19 23:38:52 -0400

    Disable console.log during tests

--- test/setupTests.js
+++ test/setupTests.js
@@ -2,3 +2,6 @@ import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
+
+// Disable console.log when running tests
+jest.spyOn(global.console, 'log').mockImplementation(() => jest.fn());
