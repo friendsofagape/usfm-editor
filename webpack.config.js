53453a6d3a44c2a3d79b8929de7646de2761861f
Russell Aunger <rba@live.com>
2022-02-11 13:32:11 +0000

    Update platform deps.

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
                 ],
             },
             {
-                test: /\.js?$/,
+                test: /\.js$/,
                 loader: "source-map-loader",
                 enforce: "pre",
             },
